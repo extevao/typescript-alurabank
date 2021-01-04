37682f2b3214631f10737964d6f7d367280bf890
Estevao <estevaovasques@hotmail.com>
2021-01-03 21:34:38 -0300

    Modulo 5

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,7 +1,6 @@
 const controller = new NegociacaoController();
 
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
+$('.form')
+    .submit(controller.adiciona.bind(controller))
 
 
